22a68b3865c1800dfcfffa5a6af4485e3ff64b69
tegusi <tegusi@tegusideMacBook-Pro.local>
2017-06-22 16:12:21 +0800

    Initial Commit

---
+++ AES-parallel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AES-parallel
+//
+//  Created by tegusi on 2017/6/22.
+//  Copyright © 2017年 tegusi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
